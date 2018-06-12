1155f59674fda841cf7047644790818512e69483
enzocr <equartinoz@ucenfotec.ac.cr>
2018-06-11 23:19:58 -0600

    Codigo bisiesto refactorizado, pruebas terminadas

--- src/ac/cr/cenfotec/logica/AnnoBisiesto.java
+++ src/ac/cr/cenfotec/logica/AnnoBisiesto.java
@@ -2,16 +2,40 @@ package ac.cr.cenfotec.logica;
 
 public class AnnoBisiesto {
 
-	public boolean isBisiesto(int anno){
-		if (anno % 4 != 0){
+	public boolean isBisiesto(int anno) {
+		if (!multiploDeCuatro(anno)) {
 			return false;
-		} else if (anno % 100 != 0) {
+		} else if (multiploDeCien(anno)) {
 			return true;
-		} else if (anno % 400 == 0) {
+		} else if (multiploDeCuatrocientos(anno)) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
+	public boolean multiploDeCuatro(int anio) {
+		if (anio % 4 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean multiploDeCien(int anio) {
+		if (anio % 100 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean multiploDeCuatrocientos(int anio) {
+		if (anio % 400 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 }
--- src/ac/cr/cenfotec/logica/test/AnnoBisiestoTest.java
+++ src/ac/cr/cenfotec/logica/test/AnnoBisiestoTest.java
@@ -16,32 +16,30 @@ public class AnnoBisiestoTest {
 		System.out.println("Inicializando");
 		anio = new AnnoBisiesto();
 	}
-
-	/*
-	 * 
-	 * @Test public void itIsBisiesto() { assertTrue("Es bisiesto",
-	 * anio.isBisiesto(2000)); System.out.println("Es bisiesto"); }
-	 * 
-	 * @Test public void itIsNotBisiesto() { assertFalse("No es bisiesto",
-	 * anio.isBisiesto(2005)); System.out.println("Es bisiesto"); }
-	 */
 	
 	@Test
+	public void isBisiesto() {
+		assertTrue(anio.multiploDeCien(2100));
+		assertFalse(anio.multiploDeCuatro(1955));
+	}
+
+	@Test
 	public void multipleOf4() {
-		assertTrue(anio.isBisiesto(1996));
-		assertFalse(anio.isBisiesto(1997));
+		assertTrue(anio.multiploDeCuatro(1996));
+		assertFalse(anio.multiploDeCuatro(1997));
 	}
 
 	@Test
 	public void multipleOf4And100() {
-		assertFalse(anio.isBisiesto(1900));
-		assertFalse(anio.isBisiesto(2100));
+		assertTrue(anio.multiploDeCuatro(1900));
+		assertTrue(anio.multiploDeCien(2100));
 	}
 
 	@Test
 	public void multipleOf400() {
-		assertTrue(anio.isBisiesto(1600));
-		assertTrue(anio.isBisiesto(2000));
+		assertTrue(anio.multiploDeCuatrocientos(1600));
+		assertTrue(anio.multiploDeCuatrocientos(2000));
+
 	}
 
 }
